6abc88d9ee31588c49eab87a552dfcc2f69ae05b
cato12 <cristhiantasayco02@gmail.com>
2025-05-20 09:17:37 -0500

    segundo intento

--- public/script.js
+++ public/script.js
@@ -1,24 +1,21 @@
 const pwdInput = document.getElementById('password');
 const toggle = document.getElementById('togglePassword');
 
-// 1) Mostrar/ocultar el icono según haya texto
+
 pwdInput.addEventListener('input', () => {
     if (pwdInput.value.length > 0) {
         toggle.style.display = 'block';
     } else {
         toggle.style.display = 'none';
-        // Asegura que vuelva a “password” al borrar
         pwdInput.type = 'password';
-        // opcional: restablecerSVG a ojo cerrado si cambiaste
     }
 });
 
-// 2) Alternar visibilidad de la contraseña
 toggle.addEventListener('click', () => {
     const isPwd = pwdInput.type === 'password';
     pwdInput.type = isPwd ? 'text' : 'password';
-    // Si quieres cambiar el icono (ojo abierto/cerrado), aquí lo haces:
-    
+
+    //CAMBIO DE ICONO OJO
     // toggle.innerHTML = isPwd
     //   ? '<img src="./img/ojo.png" alt="ojo_abierto" />'
     //   : '<img src="ojo.png" alt="ojo abierto" />';
